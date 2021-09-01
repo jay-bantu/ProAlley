0b6ab4e16123f246cab5d09a8989b7e87f6e1b31
jaybantu <jayrgeezy@gmail.com>
2021-09-01 11:13:59 +0300

    header, footer setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+
 
 function App() {
   return (
-    <div className="App">
-      Welcome to ProAlley
-    </div>
+    <>
+    <Header/>
+    
+    <main className="py-3">
+      <Container>
+         <h1>Welcome to ProAlley</h1>
+      </Container>
+    </main>
+    <Footer/>
+    </>
   );
 }
 
