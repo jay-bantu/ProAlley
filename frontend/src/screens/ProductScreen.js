ef22e8267ca7342545f29cd1bcbf30969f35f9f4
jaybantu <jayrgeezy@gmail.com>
2021-09-02 10:16:18 +0300

    fetching products homescreen and product screen from backend using axios

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import axios from 'axios'
+import React ,{useState, useEffect} from 'react'
 import { Col, Row, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import Rating from '../components/Rating'
-import products from '../products'
 
 const ProductScreens = ({match}) => {
-    const product = products.find((p)=> p._id === match.params.id)
+    const [product, setProduct] = useState({})
+
+
+    useEffect(()=>{
+        
+        const fetchProduct = async ()=>{
+
+            const {data} = await axios.get(`/api/products/${match.params.id} `)
+            setProduct(data)
+        } 
+        fetchProduct()
+
+    }, [])
+    
     return (
         <>
         <Link className='btn btn-light my-3' to='/'>Go Back</Link>
